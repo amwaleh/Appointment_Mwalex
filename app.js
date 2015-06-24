d1795d5e6db8ca16fe95f6dd282c8a7badc21f70
unknown <alexmuturi@gmail.com>
2015-06-24 23:56:49 +0300

    Added code for retrieve

--- app.js
+++ app.js
@@ -3,38 +3,38 @@ var ref = new Firebase("https://mwaleh001.firebaseio.com/");
  highestPriority = 0;
 
 var setAppointment = function() {
-  var parents= $("#appdate").val();
-  var childref=ref.child(parents);//this is the parent node :Date
+  var parents = $("#appdate").val();
+   var childref = ref.child(parents);//this is the parent node :Date
     var tasks = $("#task").val();
       var start = $("#starttime").val();
        var title = $("#title").val();
-        var finish= $("#finishtime").val();  
+        var finish = $("#finishtime").val();  
 			
-childref.child(title).setWithPriority(true,highestPriority+500)	
+childref.child(title).setWithPriority(true,highestPriority + 500)	
 
  childref.child(title).set({
-  Start: start,		
-   tasks:	tasks,
-    Title:	title,
-     Finish: finish
-      });
-      };
-
+ 	Start:start,
+ 	 tasks:tasks,
+ 	  Title:title,
+ 	   Finish:finish
+ 	});
+};
+//retrieve data using for specific date
 function getAppointment(){
  ref.on("value", function (snapshot) {
   var adate = document.getElementById("appdate").value;
    var div = document.getElementById('show');;
     var a = snapshot.val();
      var firstnod = a[adate]; 
-      div.innerHTML='<div class="Events">';
+      div.innerHTML ='<div class="Events">';
 //gets the title of the task 
 for ( title in firstnod) {
- div.innerHTML = div.innerHTML+'<div class="title"> '+ title.toUpperCase();
+ div.innerHTML = div.innerHTML +'<div class="title"> '+ title.toUpperCase();
 
 //gets propertis firstnod[title][pro]= value of property
 
   for (pro in firstnod[title]){ 
-   div.innerHTML = div.innerHTML + '<div  class="property"><li>'+pro +'</li> <li>'+ firstnod[title][pro] +'</li></div>';
+   div.innerHTML = div.innerHTML + '<div  class="property"><li>' + pro +'</li> <li>'+ firstnod[title][pro] +'</li></div>';
 	}
      div.innerHTML = div.innerHTML +'</div>'
       }
