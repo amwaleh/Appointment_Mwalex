d5457fd9bae51f653e200ba914a3a59366b5c7d4
unknown <alexmuturi@gmail.com>
2015-06-25 08:19:09 +0300

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,47 +1,43 @@
 
 var ref = new Firebase("https://mwaleh001.firebaseio.com/");
-    
-highestPriority = 0;
+ highestPriority = 0;
 
 var setAppointment = function() {
-  var parents= $("#appdate").val();
-  var childref=ref.child(parents);//this is the parent node :Date
-
- var tasks 	= $("#task").val();
- var start 	= $("#starttime").val();
- var title 	= $("#title").val();
- var finish	= $("#finishtime").val();  
+  var parents = $("#appdate").val();
+   var childref = ref.child(parents);//this is the parent node :Date
+    var tasks = $("#task").val();
+      var start = $("#starttime").val();
+       var title = $("#title").val();
+        var finish = $("#finishtime").val();  
 			
-childref.child(title).setWithPriority(true,highestPriority+500)	
+childref.child(title).setWithPriority(true,highestPriority + 500)	
 
  childref.child(title).set({
- Start: start,		
- tasks:	tasks,
- Title:	title,
- Finish: finish
- });
+ 	Start:start,
+ 	 tasks:tasks,
+ 	  Title:title,
+ 	   Finish:finish
+ 	});
 };
-
+//retrieve data using for specific date
 function getAppointment(){
-ref.on("value", function (snapshot) {
- var adate = document.getElementById("appdate").value;
- var div = document.getElementById('show');;
- var a = snapshot.val();
- var firstnod = a[adate]; 
-div.innerHTML='<div class="Events">';
+ ref.on("value", function (snapshot) {
+  var adate = document.getElementById("appdate").value;
+   var div = document.getElementById('show');;
+    var a = snapshot.val();
+     var firstnod = a[adate]; 
+      div.innerHTML ='<div class="Events">';
+//gets the title of the task 
+for ( title in firstnod) {
+ div.innerHTML = div.innerHTML +'<div class="title"> '+ title.toUpperCase();
 
- for ( title in firstnod) {//gets the title of the task 
- div.innerHTML= div.innerHTML+'<div class="title"> '+ title.toUpperCase();
+//gets propertis firstnod[title][pro]= value of property
 
-	for (pro in firstnod[title]){ //gets propertis firstnod[title][pro]= value of property
-
-	div.innerHTML= div.innerHTML + '<div  class="property"><li>'+pro +'</li> <li>'+ firstnod[title][pro] +'</li></div>';
+  for (pro in firstnod[title]){ 
+   div.innerHTML = div.innerHTML + '<div  class="property"><li>' + pro +'</li> <li>'+ firstnod[title][pro] +'</li></div>';
 	}
- div.innerHTML= div.innerHTML+'</div>'
-  }
-div.innerHTML= div.innerHTML+'</div>'
-
-
-});
-
-}
+     div.innerHTML = div.innerHTML +'</div>'
+      }
+       div.innerHTML = div.innerHTML +'</div>'
+        });
+         }
