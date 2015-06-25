4df41a413d33d9938ac40f14abd9a25503c7ff91
unknown <alexmuturi@gmail.com>
2015-06-25 17:23:35 +0300

    Finished most functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var ref = new Firebase("https://mwaleh001.firebaseio.com/");
  highestPriority = 0;
 
+
 var setAppointment = function() {
   var parents = $("#appdate").val();
   var childref = ref.child(parents);//this is the parent node :Date
@@ -10,7 +11,16 @@ var setAppointment = function() {
   var finish = $("#finishtime").val();
   var priority = $("#priority").val();
 			
-childref.child(title).setWithPriority(true,highestPriority + priority)	
+childref.child(title).on( "value", function (snapshot){
+  if(snapshot){
+    alert("the following event already exist choose unique title");
+    getAppointment();
+  console.log (snapshot.val());
+}
+
+})
+
+/*childref.child(title).setWithPriority(priority, priority)	
 
  childref.child(title).set({
  start:start,
@@ -18,7 +28,7 @@ childref.child(title).setWithPriority(true,highestPriority + priority)
  title:title,
  finish:finish,
  priority:priority
- 	});
+ 	});*/
 };
 //retrieve data using for specific date
 function getAppointment(){
@@ -37,7 +47,7 @@ for ( title in firstnod) {
 //gets propertis firstnod[title][pro]= value of property
 
   for (pro in firstnod[title]){ 
-    var childtext = "\""+adate +"/"+ title+"\"".toString();;
+    var childtext = "\""+adate +"\",\""+ title+"\"".toString();
     icons="<a ><img id='edit' src='images/edit.png' onclick='editChildky(" + childtext +")';'><img id='edit' src='images/delete.png' onclick='removeChildky(" + childtext +")';'></a>"
    div.innerHTML = div.innerHTML +'<div  class="property"><li>' + pro +'</li> <li> '+ firstnod[title][pro] +'</li></div>';
 	}
@@ -80,18 +90,61 @@ div.innerHTML = div.innerHTML +'</div>'
 //check if appointment exist
 
 //
-function removeChildky(childnode){
+function removeChildky(parent,child){
+  var childnode=parent +"/"+ child
   //childon=childon.replace('!','/').toString
-  ref.child(childon).remove();
-  console.log(childon);
+  ref.child(childnode).remove();
+  console.log(childnode);
   //
-}
-function editChildky(childnode){
-  ref.child(childnode).on("value", function (snapshot) {
-    //for( key in snapshot.val()){
-      console.log(snapshot.val()["finish"]);
-   // }
-  //
-});
 }
 
+var updatenode=''
+function editChildky(parent,child){
+  var childnode=parent +"/"+ child
+ updatenode=childnode;
+  var table=document.getElementById('form');
+  ref.child(childnode).on("value", function (snapshot) {
+    var open ="<tr>";
+    
+    document.getElementById('appdate').value=parent;
+    document.getElementById('title').value=child;
+    document.getElementById('starttime').value=snapshot.val()['start'];
+    
+    document.getElementById('finishtime').value=snapshot.val()['finish'];
+     
+    document.getElementById('priority').value=snapshot.val()['priority'];
+    document.getElementById('task').value=snapshot.val()['tasks'];
+
+  })
+
+  
+ 
+}
+
+
+function updatechild(){
+  if(!updatenode) {
+    alert('cannot alert')
+    return;}
+  var parents = $("#appdate").val();
+  var childref = ref.child(parents);//this is the parent node :Date
+  var tasks = $("#task").val();
+  var start = $("#starttime").val();
+  var title = $("#title").val();
+  var finish = $("#finishtime").val();
+  var priority = $("#priority").val();
+console.log(updatenode);
+
+ 
+
+  ref.child(updatenode).update({
+    start:start,
+    tasks:tasks,
+    title:title,
+    finish:finish,
+    priority:priority
+  });
+
+
+
+}
