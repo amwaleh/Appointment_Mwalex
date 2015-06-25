0e99da0c5b8c04bb1d59639728cecfc64a80c571
unknown <alexmuturi@gmail.com>
2015-06-25 13:54:53 +0300

    delete todo

--- js/app.js
+++ js/app.js
@@ -1,33 +1,65 @@
-
 var ref = new Firebase("https://mwaleh001.firebaseio.com/");
  highestPriority = 0;
 
 var setAppointment = function() {
   var parents = $("#appdate").val();
-   var childref = ref.child(parents);//this is the parent node :Date
-    var tasks = $("#task").val();
-      var start = $("#starttime").val();
-       var title = $("#title").val();
-        var finish = $("#finishtime").val();  
+  var childref = ref.child(parents);//this is the parent node :Date
+  var tasks = $("#task").val();
+  var start = $("#starttime").val();
+  var title = $("#title").val();
+  var finish = $("#finishtime").val();
+  var priority = $("#priority").val();
 			
-childref.child(title).setWithPriority(true,highestPriority + 500)	
+childref.child(title).setWithPriority(true,highestPriority + priority)	
 
  childref.child(title).set({
- 	Start:start,
- 	 tasks:tasks,
- 	  Title:title,
- 	   Finish:finish
+ start:start,
+ tasks:tasks,
+ title:title,
+ finish:finish,
+ priority:priority
  	});
 };
 //retrieve data using for specific date
 function getAppointment(){
- ref.on("value", function (snapshot) {
-  var adate = document.getElementById("appdate").value;
-   var div = document.getElementById('show');;
-    var a = snapshot.val();
-     var firstnod = a[adate]; 
-      div.innerHTML ='<div class="Events">';
+ref.orderByPriority().on("value", function (snapshot) {
+ var adate = document.getElementById("appdate").value;
+ var div = document.getElementById('show');;
+ var a = snapshot.val();
+ var firstnod = a[adate]; 
+
+div.innerHTML ='<div class="Events">';
+var icons='';
 //gets the title of the task 
+for ( title in firstnod) {
+ div.innerHTML = div.innerHTML +'<div class="title">'+ title.toUpperCase();
+
+//gets propertis firstnod[title][pro]= value of property
+
+  for (pro in firstnod[title]){ 
+    var childtext = "\""+adate +"/"+ title+"\"".toString();;
+    icons="<a ><img id='edit' src='images/edit.png' onclick='editChildky(" + childtext +")';'><img id='edit' src='images/delete.png' onclick='removeChildky(" + childtext +")';'></a>"
+   div.innerHTML = div.innerHTML +'<div  class="property"><li>' + pro +'</li> <li> '+ firstnod[title][pro] +'</li></div>';
+	}
+  div.innerHTML = div.innerHTML + icons+'</div>'
+}
+       
+  div.innerHTML = div.innerHTML +'</div>'
+});
+}
+
+
+function getAllAppointment(){
+ ref.on("value", function (snapshot) {
+  
+  var div = document.getElementById('show');;
+  var a = snapshot.val();
+     
+  div.innerHTML ='<div class="Events">';
+//gets the title of the task 
+for (adate in a){
+ var firstnod = a[adate]; 
+ div.innerHTML = div.innerHTML +'<div class="title"> '+ adate.toUpperCase()+'</date>';
 for ( title in firstnod) {
  div.innerHTML = div.innerHTML +'<div class="title"> '+ title.toUpperCase();
 
@@ -35,9 +67,31 @@ for ( title in firstnod) {
 
   for (pro in firstnod[title]){ 
    div.innerHTML = div.innerHTML + '<div  class="property"><li>' + pro +'</li> <li>'+ firstnod[title][pro] +'</li></div>';
-	}
-     div.innerHTML = div.innerHTML +'</div>'
-      }
-       div.innerHTML = div.innerHTML +'</div>'
-        });
-         }
+  }
+   div.innerHTML = div.innerHTML +'</div>'
+}
+div.innerHTML = div.innerHTML +'</div>'
+}
+
+});
+
+}
+
+//check if appointment exist
+
+//
+function removeChildky(childnode){
+  //childon=childon.replace('!','/').toString
+  ref.child(childon).remove();
+  console.log(childon);
+  //
+}
+function editChildky(childnode){
+  ref.child(childnode).on("value", function (snapshot) {
+    //for( key in snapshot.val()){
+      console.log(snapshot.val()["finish"]);
+   // }
+  //
+});
+}
+
